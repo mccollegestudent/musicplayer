371ac065dc9cecaab4a2bf6ec8234b9eed9a21e6
mccollegestudent <94277576+mccollegestudent@users.noreply.github.com>
2022-04-18 19:41:23 -0400

    Update musicPlayer.php
    upadated musicPlayer

--- musicPlayer.php
+++ musicPlayer.php
@@ -10,102 +10,257 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 ?>
  
  <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>CodePen - Material Music Player</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.css'>
-<link rel='stylesheet' href='https://owlcarousel2.github.io/OwlCarousel2/assets/owlcarousel/assets/owl.carousel.min.css'><link rel="stylesheet" href="./style.css">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>modern music player</title>
+	<link rel="stylesheet" href="style.css">
+	<link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<!-- where bx and zmdi pull their resourses -->
+	<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.css'>
+	<link rel='stylesheet' href='https://owlcarousel2.github.io/OwlCarousel2/assets/owlcarousel/assets/owl.carousel.min.css'><link rel="stylesheet" href="./style.css">
+
 
 </head>
+
+<style>
+    th{ 
+		width: 80%;
+	    height: 100%;
+        cursor: pointer;
+        color:#fff;
+     }
+     #d1{ /*-- scrowbar */
+		width: 80%;
+	    height: 100%;
+        overflow:scroll;
+        overflow-x:hidden;
+     }
+</style>
+
 <body>
-<div class="hero__backgorund owl-carousel">
-		<div class="item" style="background: #3F51B5"></div>
-		<div class="item" style="background: #8BC34A"></div>
-		<div class="item" style="background: #673AB7"></div>
-		<div class="item" style="background: #E91E63"></div>
-</div>
 
 
 
-	<div class="main__container">
-		<div class="contain__wrapper">
-			<div class="music__wrapper">
-				<div class="left__panel">
-					<div class="cover owl-carousel">
-					    <div class="item">
-					    	<img src="http://cdn.pikoff.com/wp-content/uploads/2016/05/02/Ai-Pink-and-purple-abstract-background-vector-free-download-350x350.jpg" alt="">
-					    </div>
-					    <div class="item">
-					    	<img src="http://cdn.pikoff.com/wp-content/uploads/2016/05/02/Ai-Pink-and-orange-abstract-background-vector-free-download-350x350.jpg" alt="">
-					    </div>
-					    <div class="item">
-					    	<img src="http://cdn.pikoff.com/wp-content/uploads/2016/05/02/Ai-Blue-abstract-background-vector-free-download-350x350.jpg" alt="">
-					    </div>
-					    <div class="item">
-					    	<img src="http://cdn.pikoff.com/wp-content/uploads/2016/05/02/Ai-Yellow-and-green-abstract-background-vector-free-download-350x350.jpg" alt="">
-					    </div>
-					</div>
+
+</p>
+	<div class="main">
+		
+<!-- top bar-->
+		<div class="top_bar">
+			<button onclick="sidebar()"><i class='bx bx-chevron-right'></i></button> <!-- top left button brings nested buttuns-->
+			<div class="options">
+
+				<button><i class='bx bxs-volume-low' ></i></button> <!-- class calls icons from ipi etc pres - or follow link to see icons available-->
+				<button name="editPlaylistBtn" onclick="open_playlist()"><i class='bx bx-edit-alt'></i></button>	<!-- add songs to current playlist-->			
+				<button><i class='zmdi zmdi-account' ></i></button>			
+			
+				
+			</div>
+			<button name="playlistsBtn" onclick="open_playlists()"><i class='bx bxs-playlist' ></i></button>
+		</div>
+
+		<div class="playing_part">
+			<div class="img">
+				<img src="bg.jpg" alt="img">
+			</div>
+		</div>
+
+<!-- control part -->
+		<div class="control_part">
+			<div class="song_title">
+				<p>song title</p>
+				<p>artist name</p>
+			</div>
+
+			<div class="control_buttons">
+				<div class="range_slider">
+					<input type="range" min="0" max="100" id="slider" value="0">
+					<p>3:00</p>
 				</div>
 
-				<div class="right__panel">
-					<div class="music__info__wrapper">
-						<div class="header">
-							<div class="icon__wrapper">
-								
-								<span class="icon-right"><i class="zmdi zmdi-apps"></i></span>
-								<div class="title owl-carousel">
-								    <div class="item">
-										<div class="song__name">Sorry The Movement</div>
-										<p class="album__name">Justin Bieber</p>
-								    </div>
-								    <div class="item">
-										<div class="song__name">Passenger Let Her Go</div>
-										<p class="album__name">Passenger</p>
-								    </div>
-								    <div class="item">
-										<div class="song__name">Counting Stars...</div>
-										<p class="album__name">OneRepublic</p>
-								    </div>
-								    <div class="item">
-										<div class="song__name">Waiting For Love</div>
-										<p class="album__name">Avicii</p>
-								    </div>
-								</div>
-								
-							</div>
-						</div>
-
-						<div class="music__control">
-							<div class="music__button">
-								<span class="button__prev"><i class="zmdi zmdi-fast-rewind"></i></span>
-								<span class="button__pause"><i class="zmdi zmdi-pause"></i></span>
-								<span class="button__next"><i class="zmdi zmdi-fast-forward"></i></span>
-								<div>
-									<input type="range" min="0" max="50" value="10" step="1" />	
-								</div>
-								
-							</div>
-						</div>
-					</div>
-					<div class="music__menu">
-						<span class="icon-right"><i class="zmdi zmdi-close"></i></span>
-						<span class="menu_list"><i class="zmdi zmdi-favorite"></i>Faverate</span>
-						<span class="menu_list"><i class="zmdi zmdi-audio"></i>Music</span>
-						<span class="menu_list"><i class="zmdi zmdi-view-carousel"></i>Album</span>
-						<span class="menu_list"><i class="zmdi zmdi-time-restore"></i>History</span>
-						<span class="menu_list"><i class="zmdi zmdi-shopping-cart"></i>Cart</span>
-						<a href="logout.php"><span class="menu_list"><i class="zmdi zmdi-settings"></i>logout</span></a>
-					</div>
-
+				<div class="main_btns">
+					<button id="loop"><i class='zmdi zmdi-repeat' ></i></button>
+					<button id="play_btn"><i class='bx bx-play' ></i></button>
+					<button id="next"><i class='bx bx-skip-next' ></i></button>
 				</div>
 			</div>
 		</div>
-	</div>
-<!-- partial -->
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js'></script>
-<script src='https://owlcarousel2.github.io/OwlCarousel2/assets/owlcarousel/owl.carousel.js'></script><script  src="./script.js"></script>
 
+<!-- playlist songs -->
+		<div class="playlist">  <!-- change this to show only playlist names routed to the edit button-->
+			
+			<p style="font-size:x-large"  style="color:orangered;">:Current Paylist Name</p>	
+			<div class = "row">
+				<div class = "col"> 
+					<div class = "card body">
+						<input size="55" height="200" id = "searchBtn" class = "form control" type = "text" style="color: black;">
+						<br><br>
+					</div>
+				</div>
+			</div>	
+			
+			<div id = "d1">
+				<table class="table table-striped" style="width: 120%;">
+					<tr>
+						<tbody id="myTable">
+							
+							
+						<!-- formart used in script for entery (id="myTable") line 234			
+							<div class="p_song active_song">
+								<p id="p_title">song name</p>
+								<p id="p_artist">artist name</p>
+								<button ><i class='bx bx-minus' ></i></button>
+							</div>
+						-->
+						<tbody>
+					</tr>
+				</table>
+	
+			</div>
+
+
+			<div class="p_song active_song">   <!--last panel on playlit with back and add buttons-->
+				<button onclick="open_playlist()"><i class='zmdi zmdi-arrow-back'></i></button>
+				<button id="addToPlaylistBtn"><i class= 'bx bxs-plus-circle' ></i></button>
+			</div>
+		
+
+				
+		</div>
+
+<!-- similar design for iterating for [music Library but with filtering options + search bar]-->
+
+		<div class="playlists">
+
+			<div class="p_song active_song">
+				<p id="p_title">playlist name</p>
+				<button><i class='bx bx-minus' ></i></button>
+				
+			</div>
+
+			
+			<div class="p_song active_song">
+				<p id="p_title">playlist name</p>
+				<button><i class='bx bx-minus' ></i></button>
+				
+			</div>
+
+			
+			<div class="p_song active_song">
+				<p id="p_title">playlist name</p>
+				<button><i class= 'bx bx-minus' ></i></button>
+			</div>
+		
+
+			<div class="p_song active_song">
+				<button onclick="open_playlists()"><i class='zmdi zmdi-arrow-back'></i></button>
+				<button><i class= 'bx bxs-plus-circle' ></i></button>
+
+			</div>
+		</div>
+						
+	</div>
+
+<!--  javascript switching pages -->
+
+	   <script>
+		let playlist = document.querySelector('.playlist');
+		let playlists = document.querySelector('.playlists');
+		let options = document.querySelector('.options');
+
+		function open_playlist(){
+
+			playlist.classList.toggle('active');
+	
+			//document.getElementById("playlistsBtn").disabled = true;
+		}
+
+		function open_playlists(){
+			//playlist.classList.toggle('active');
+			playlists.classList.toggle('active3');
+		
+		
+		
+		}
+
+		function open_removeSong(index){
+
+			console.log('------------------song removed:')
+			currPlaylistArray.splice(index,1)
+			buildTable(currPlaylistArray)
+		}
+
+		function sidebar(){
+			options.classList.toggle('active2');
+		}
+
+
+	//<!--  javascript a particular playlist -->
+
+	
+		var currPlaylistArray = [
+			{'name':'aad', 'artist':'Michael','album':'BadAlbum','genre':'POP', 'year':'1989'},
+			{'name':'baw', 'artist':'Royce','album':'chart','genre':'Hip Hop', 'year':'2010'},
+			{'name':'cad', 'artist':'Michael','album':'BadAlbum','genre':'POP', 'year':'1989'},
+			{'name':'daw', 'artist':'Royce','album':'chart','genre':'Hip Hop', 'year':'2010'},
+			{'name':'ead', 'artist':'Michael','album':'BadAlbum','genre':'POP', 'year':'1989'},
+			{'name':'faw', 'artist':'Royce','album':'chart','genre':'Hip Hop', 'year':'2010'},
+			{'name':'Bad', 'artist':'Michael','album':'BadAlbum','genre':'POP', 'year':'1989'},
+			{'name':'Raw', 'artist':'Royce','album':'chart','genre':'Hip Hop', 'year':'2010'},
+		 
+		]
+		
+		
+		$('#searchBtn').on('keyup', function(){
+			
+			var value = $(this).val()
+			console.log('value:', value)
+			var data = searchTable(value, currPlaylistArray)
+			buildTable(data)
+		
+		
+		})
+		
+		buildTable(currPlaylistArray)
+		
+		function searchTable(value, data){
+			var filterData = []
+			for(var i = 0; i < data.length; i++){
+		
+				value = value.toLowerCase()
+				var name = data[i].name.toLowerCase()
+			  
+				if(name.includes(value)){
+					filterData.push(data[i])         
+				}
+			}
+			return filterData
+		}
+			
+		function buildTable(data){ //the table build it will be updated when an event is invoked etc searching table
+			var table = document.getElementById('myTable')
+			table.innerHTML = ''
+			for (var i = 0; i < data.length; i++){
+
+				/*basically html code in javascript variable automates records  addToPlaylistBtn deletes an element form array passes index to fumction */
+				var row ='<div class="p_song active_song">'+
+								'<p id="p_title">' + data[i].name +'</p>'+
+								'<p id="p_artist">'+ "Artist : " + data[i].artist +'</p>'+
+								'<p id="p_album">' + "Album : "  + data[i].album  +'</p>'+
+								'<p id="p_genre">' + "Genre : "          + data[i].genre  +'</p>'+
+								'<p id="p_year">'  + ""          + data[i].year   +'</p>'+
+								'<button id="addToPlaylistBtn" onclick="open_removeSong('+i+')">' + '<i class="bx bx-minus" ></i></button>'+
+						 '</div>';
+
+						 
+				table.innerHTML += row
+			}
+		}
+		
+		</script>
+	
 </body>
 </html>
